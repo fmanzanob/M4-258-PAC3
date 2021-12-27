42e1e2411a342f185051ed4ea2575a26589a7ef7
fmanzanob <61649066+fmanzanob@users.noreply.github.com>
2021-12-27 22:24:04 +0100

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,6 @@
+// Example `tailwind.config.js` file
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: [
     './src/**/*.html',
@@ -5,15 +8,27 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    container:{
+      center:true,
+    },
     colors: {
       gray: colors.coolGray,
       blue: colors.lightBlue,
       red: colors.rose,
       pink: colors.fuchsia,
+      yellow: colors.yellow,
+      white: colors.white,
     },
     fontFamily: {
       sans: ['Graphik', 'sans-serif'],
       serif: ['Merriweather', 'serif'],
+    },
+    maxHeight: {
+      '0': '0',
+      '1/4': '25%',
+      '1/2': '50%',
+      '3/4': '75%',
+      'full': '100%',
     },
     extend: {
       spacing: {
@@ -32,10 +47,5 @@ module.exports = {
       opacity: ['disabled'],
     }
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/typography'),
-    require('tailwindcss-children'),
-  ],
+  plugins: [],
 }
